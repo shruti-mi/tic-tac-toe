a396a84add39d88e7f439bd9aff7404b6825cd0d
shrutimittal1100 <shrutimittal1100@gmail.com>
2020-06-27 20:56:34 +0530

    Updates

---
+++ precache-manifest.baf0248c848bd243d220aaeae51d3086.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c0b4e55df563ae54c56c446f23f344c",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "00c43f7f2cdb207eb37e",
+    "url": "/tic-tac-toe/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "333515a4d31e42dba7dd",
+    "url": "/tic-tac-toe/static/js/2.d51d0453.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac-toe/static/js/2.d51d0453.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00c43f7f2cdb207eb37e",
+    "url": "/tic-tac-toe/static/js/main.26546207.chunk.js"
+  },
+  {
+    "revision": "7f32745e7b69030faee7",
+    "url": "/tic-tac-toe/static/js/runtime-main.df28867d.js"
+  }
+]);
